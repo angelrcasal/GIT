1e21856503a8f626b0ad1db20f5ee18563511a75
angcas <angcas@greeny.cs.tlu.ee>
2016-02-18 11:38:23 +0200

    labels and inouts

--- app.php
+++ app.php
@@ -1,14 +1,29 @@
-< ?php
+<?php
 
+      //getting the message from address
+      //if there is ?name= .. then $_GET["name"]
+      $my_message = $_GET["message"];
+      $to = $_GET["to"];
+      $from = $_GET["from"];
 
+      echo "My message is ".$my_message." and is to".$to;
     
 ?>
     
 
 
-    <form method="get">    
-    <label for ="message"> message:* <label><br>
-    <imput type ="text" name="message"><br>
-    <imput type ="submit" value="save to DB">
+    
+<form method="get">  
+    <label for ="from"> from:* </label><br>
+    <input type ="text" name="from"><br><br>
+    
+    <label for ="to"> to:* </label><br>
+    <input type ="text" name="to"><br><br>
         
-    <form>    
+    <label for ="message"> message:* </label><br>
+    <input type ="text" name="message"><br><br>
+        
+    <this is the save button-->
+    <input type="submit" value="save to DB"
+        
+    </form>    
