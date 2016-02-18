b6d7af9a8ca3615bd65110ddf5fbc94fba4fa06f
angcas <angcas@greeny.cs.tlu.ee>
2016-02-18 11:06:39 +0200

    added form

---
+++ app.php
@@ -0,0 +1,14 @@
+< ?php
+
+
+    
+?>
+    
+
+
+    <form method="get">    
+    <label for ="message"> message:* <label><br>
+    <imput type ="text" name="message"><br>
+    <imput type ="submit" value="save to DB">
+        
+    <form>    
